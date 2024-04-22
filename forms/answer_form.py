193f68f3af95c24f901b6f2f03f73e20704d0966
RyuMinSoo <sonicus94.dev@gmail.com>
2024-04-22 11:32:55 +0900

    answer update

--- forms/answer_form.py
+++ forms/answer_form.py
@@ -5,3 +5,6 @@ from wtforms.validators import DataRequired
 
 class AnswerCreateForm(FlaskForm):
     content = TextAreaField("답변내용", validators=[DataRequired()])
+
+class AnswerUpdateForm(FlaskForm):
+    content = TextAreaField("답변내용", validators=[DataRequired()])
--- views/answer_views.py
+++ views/answer_views.py
@@ -1,14 +1,34 @@
 from datetime import datetime
-from flask import Blueprint, jsonify, redirect, request, url_for
+from flask import Blueprint, flash, jsonify, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
 from app import db
-from forms.answer_form import AnswerCreateForm
+from forms.answer_form import AnswerCreateForm, AnswerUpdateForm
 from models.answer_model import Answer
 from models.question_model import Question
 
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
+
+
+@bp.route('/delete/<int:answer_id>')
+def delete(answer_id):
+    return jsonify(success=True)
+
+
+@bp.route('/update/<int:answer_id>', methods=["GET", "POST"])
+def update(answer_id):
+    answer = Answer.query.get_or_404(answer_id)
+    form = AnswerUpdateForm(obj=answer) #원래 내용 가져오기
+    #post and validate
+    if request.method == 'POST' and form.validate_on_submit():
+        form.populate_obj(answer)
+        db.session.commit() #db저장
+        return redirect(url_for('question.detail', question_id=answer.question.id))
+    #get
+    if current_user.nickname != answer.user.nickname: #권한있는 사람만 url로 접근가능
+        return jsonify(grant=False)
+    return render_template('answer/update.html', form=form)
 
 
 @bp.route('/create/<int:question_id>', methods=["POST"])
