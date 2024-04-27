bb3fb8b5e803bb1809a5207703659c9cc2363ed1
RyuMinSu <sonicsu94@gmail.com>
2024-04-27 15:10:39 +0900

    question voter and answer voter

--- models/answer_model.py
+++ models/answer_model.py
@@ -2,6 +2,12 @@ from app import db
 from models.user_model import User
 
 
+answer_voter = db.Table(
+    'answer_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('answer_id', db.Integer, db.ForeignKey('answer.id', ondelete='CASCADE'), primary_key=True)
+)
+
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text(), nullable=False)
@@ -10,3 +16,4 @@ class Answer(db.Model):
     question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
     user = db.relationship('User', backref=db.backref('answer_set'))
     question = db.relationship('Question', backref=db.backref('answer_set', order_by='Answer.created_at.desc()'))
+    voter = db.relationship('User', secondary=answer_voter, backref=db.backref('answer_voter_set'))
--- models/question_model.py
+++ models/question_model.py
@@ -1,5 +1,12 @@
+from sqlalchemy import ForeignKey
 from app import db
 
+
+question_voter = db.Table(
+    'question_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), primary_key=True),
+    db.Column('question_id', db.Integer, db.ForeignKey('question.id', ondelete="CASCADE"), primary_key=True)
+)
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +16,4 @@ class Question(db.Model):
     modified_at = db.Column(db.DateTime(), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
     user = db.relationship('User', backref=db.backref('question_set'))
+    voter = db.relationship('User', secondary=question_voter, backref=db.backref('question_voter_set'))
