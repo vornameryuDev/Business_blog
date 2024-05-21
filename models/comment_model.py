e0a1bfff7b540351f921d4cc61dab900c4dbb2a8
RyuMinSoo <sonicus94.dev@gmail.com>
2024-05-21 14:49:30 +0900

    modified question comment

--- models/comment_model.py
+++ models/comment_model.py
@@ -1,6 +1,12 @@
 from app import db
 
 
+comment_voter = db.Table(
+    'comment_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('comment_id', db.Integer, db.ForeignKey('comment.id', ondelete='CASCADE'), primary_key=True)
+)
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,3 +19,4 @@ class Comment(db.Model):
     content = db.Column(db.Text(), nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False)
     modified_at = db.Column(db.DateTime())
+    voter = db.relationship('User', secondary=comment_voter, backref=db.backref('comment_voter_set'))
--- views/comment_views.py
+++ views/comment_views.py
@@ -1,7 +1,8 @@
 import datetime
+from sre_constants import SUCCESS
 from flask_login import current_user, login_required
 from models.comment_model import Comment
-from flask import Blueprint, jsonify, redirect, request, url_for
+from flask import Blueprint, flash, jsonify, redirect, request, url_for
 
 from models.question_model import Question
 from app import db
@@ -10,16 +11,34 @@ from app import db
 bp = Blueprint('comment', __name__, url_prefix='/comment')
 
 
+@bp.route('/vote/<int:comment_id>')
+@login_required
+def vote(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    if current_user.id == comment.user_id:
+        flash('본인의 글은 추천하지 못합니다')
+    else:
+        comment.voter.append(current_user) #추천정보 db 저장
+        db.session.commit()
+    return redirect(url_for('question.detail', question_id=comment.question_id))
+
+
 @bp.route('/delete/<int:comment_id>')
-def delete(comment_id):
-    return jsonify(delete=True)
+@login_required
+def delete(comment_id):    
+    comment = Comment.query.get_or_404(comment_id)    
+    if current_user.nickname != comment.user.nickname:
+        return jsonify(Grant=False)
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect(url_for('question.detail', question_id=comment.question_id))
 
 @bp.route('/update/<int:comment_id>', methods=["POST"])
+@login_required
 def update(comment_id):
     comment = Comment.query.get_or_404(comment_id)    
-    if request.method == "POST":
-        update_comment = request.form['comment']
-        print(update_comment)
+    if request.method == "POST":        
+        update_comment = request.form['comment']        
         comment.content = update_comment
         comment.modified_at = datetime.datetime.now()
         db.session.commit()
